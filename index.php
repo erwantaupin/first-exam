e09ebd533e4664eed964a1e2332e7c806ce4f0e4
erwantaupin <taupin.erwan@laposte.net>
2022-02-28 16:29:07 +0100

    first-comit

---
+++ index.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="assets/css/style.css">
+    <script src="https://kit.fontawesome.com/aa36982253.js" crossorigin="anonymous"></script>
+    <title>Accueil</title>
+</head>
+<body>
+    <header>
+        <div class="box_button">
+            <div class="signup">sign up</div>
+            <div class="signin">sign in</div>
+        </div>
+    </header>
+    <div class="imagep">
+        <div class="titre">
+            <H1>Le Metropolis</H1><br>
+            <h4>votre site de VOD</h4>
+        </div>
+    </div>
+    <div class="container1">
+        <div class="sstitre">
+            <H2>Le Cinema comme si vous y etiez</H2>
+        </div>
+        <div class="souscontainer1">
+            <div class="image1"></div>
+            <div class="texte1"><p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quo et iusto provident ea ratione ab eligendi minus aliquid ipsa alias, voluptatum ducimus facilis at repudiandae tempora. Maiores praesentium labore quam!
+            Quo id, qui, amet assumenda animi omnis modi nesciunt vel, excepturi illum sit at neque deleniti possimus dolorum hic ab blanditiis suscipit. Accusamus, eos praesentium. Voluptatem ullam dolore adipisci ut.
+            Iure saepe dolore cupiditate iste dolorem similique, error, deleniti libero nulla maxime aliquam, quos atque eum tempora amet voluptates expedita quis eaque assumenda. Nemo, facere reprehenderit laudantium cupiditate ipsa veritatis.
+            Possimus laborum hic quia beatae corrupti! Velit illo delectus, debitis autem perspiciatis perferendis ea sapiente incidunt accusamus quisquam doloremque eaque in dignissimos voluptate necessitatibus ratione ipsam nostrum vitae eligendi? Modi?
+             repellat saepe omnis, adipisci doloribus porro alias animi, cum blanditiis.</p></div>
+        </div>
+    </div>
+    <div class="paralax"></div>
+    <div class="container2">
+        
+        <div class="souscontainer2">
+            <div class="image2"></div>
+            <div class="texte2">
+                <div class="sstitre">
+                    <H2>Metropolis</H2>
+                </div>
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quo et iusto provident ea ratione ab eligendi minus aliquid ipsa alias, voluptatum ducimus facilis at repudiandae tempora. Maiores praesentium labore quam!
+            Quo id, qui, amet assumenda animi omnis modi nesciunt vel, excepturi illum sit at neque deleniti possimus dolorum hic ab blanditiis suscipit. Accusamus, eos praesentium. Voluptatem ullam dolore adipisci ut.
+            Iure saepe dolore cupiditate iste dolorem similique, error, deleniti libero nulla maxime aliquam, quos atque eum tempora amet voluptates expedita quis eaque assumenda. Nemo, facere reprehenderit laudantium cupiditate ipsa veritatis.
+            Possimus laborum hic quia beatae corrupti! Velit illo delectus, debitis autem perspiciatis perferendis ea sapiente incidunt accusamus quisquam doloremque eaque in dignissimos voluptate necessitatibus ratione ipsam nostrum vitae eligendi? Modi?
+             repellat saepe omnis, adipisci doloribus porro alias animi, cum blanditiis.</p>
+            </div>
+        </div>
+    </div>
+    <footer>
+    <div class="copy"><h5>&copy; Copyright Metropolis</h5></div>
+            <div class="link">
+                
+                <div class="icone"><img src="assets/images/svg/twitter.svg"></div>
+                <div class="icone"><img src="assets/images/svg/facebook.svg"></div>
+                <div class="icone"><img src="assets/images/svg/instagram.svg"></div>
+            </div>
+    </footer>
+</body>
+</html>
