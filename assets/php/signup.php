1b00ff6d641337b5396939aa1ed07c951582fb3d
erwantaupin <taupin.erwan@laposte.net>
2022-03-01 16:33:30 +0100

    connex

--- assets/php/signup.php
+++ assets/php/signup.php
@@ -4,9 +4,24 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <link rel="stylesheet" href="../css/login.css">
+    <title>Login</title>
 </head>
 <body>
-    
+    <div class="box-log">
+        
+    <div class="logge"><div class="titre-log"><h2>Login</h2></div>
+            <form action="traitement.php" method="post" class="form-log">
+                <p><h3>Pseudo        : </h3><input class="input" type="text" name="pseudo" placeholder="votre pseudo" required/></p><span></span>
+                <p><h3>Mot de Passe  : </h3><input class="input" type="password" name="password" placeholder="votre password" required/></p>
+                <span></span>
+                <p><input type="checkbox" name="remember"/><H4>se souvenir de moi</H4></p>
+                <span></span>
+                <input type="submit" value="Connexion" id="subm"/>
+            </form>
+            <span></span>
+            <div class="button"><a href="../../index.php"><H2>Retour</H2></a></div>
+        </div>
+    </div>
 </body>
 </html>
