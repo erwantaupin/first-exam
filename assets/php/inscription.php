a0d1bee3542bb87fb4d70990708aea6b58e44234
erwantaupin <taupin.erwan@laposte.net>
2022-03-17 09:23:33 +0100

    inscription proteger en string vide

--- assets/php/inscription.php
+++ assets/php/inscription.php
@@ -1,18 +1,30 @@
 <?php
     require("db.php");
     if(isset($_POST)){
-        if(isset($_POST["username"])){
+
+        if(isset($_POST["username"]) && !empty($_POST["username"])){
             $name = $_POST["username"];
-        }
-        if(isset($_POST["mail"])){
-            $mail = $_POST["mail"];
-        }
-        if(isset($_POST["password"])){
-            $password = $_POST["password"];
-        }
+
+            if(isset($_POST["mail"]) && !empty($_POST["mail"])){
+                $mail = $_POST["mail"];
+
+                if(isset($_POST["password"]) && !empty($_POST["password"])){
+                    $password = $_POST["password"];
+                
+                
+                }
+                else{ header("Location: signin.php?erreur=4");
+                    }
         
-    }else{
-    }
+            }
+            else{ header("Location: signin.php?erreur=4");
+                }
+        }
+        else{ header("Location: signin.php?erreur=4");
+            }
+        
+        
+    
 
 
             $sqlRequest = "INSERT INTO users (nom_user,  mail_user, password_user,id_role)
@@ -24,4 +36,8 @@
                 ':password_user' => $password
             ));
             header("Location: signup.php?success=1");
-    
+
+        }
+        else{
+        
+        }
